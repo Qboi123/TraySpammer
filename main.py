69f86e3c83248fee79ffe83a6983d1d07d632163
Qboi123 <qboiwastaken@gmail.com>
2023-05-16 20:17:00 +0200

    Update some stuff.

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+import os
+import sys
 import webbrowser
 import wx
 from wx import adv as wx_adv
 from wx.adv import TaskBarIcon as TaskBarIcon
 
-SPAM_AMOUNT = 100
+SPAM_AMOUNT = 20
+
+APP: "TraySpammerApp"
 
 
 class SpammedTaskBarIcon(TaskBarIcon):
-    def __init__(self, frame):
+    def __init__(self, frame: "HiddenFrame"):
         TaskBarIcon.__init__(self)
 
         self.frame = frame
 
-        self.SetIcon(wx.Icon('./bitmaps/icon.png', wx.BITMAP_TYPE_PNG), 'Never Gonna Give You Up')
-        self.Bind(wx_adv.EVT_TASKBAR_LEFT_DOWN, lambda event: webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+        # noinspection PyUnusedLocal
+        def click(event):
+            webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+            frame.icon.Destroy()
+            frame.Destroy()
+
+        self.SetIcon(wx.Icon('./res/icon.png', wx.BITMAP_TYPE_PNG), 'Never Gonna Give You Up')
+        self.Bind(wx_adv.EVT_TASKBAR_LEFT_DOWN, click)
         self.Bind(wx_adv.EVT_TASKBAR_RIGHT_DOWN, lambda event: webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
 
     def CreatePopupMenu(self):
@@ -29,17 +40,17 @@ class SpammedTaskBarIcon(TaskBarIcon):
 
 
 class HiddenFrame(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, (-1, -1), (290, 280))
+    def __init__(self, parent, id_, title):
+        wx.Frame.__init__(self, parent, id_, title, (-1, -1), (290, 280))
 
         self.SetSize((350, 250))
+        self.Bind(wx.EVT_CLOSE, self.on_close)
 
         self.icon = SpammedTaskBarIcon(self)
 
-        self.Bind(wx.EVT_CLOSE, self.on_close)
-
         self.Centre()
 
+    # noinspection PyUnusedLocal
     def on_close(self, event):
         self.icon.Destroy()
         self.Destroy()
@@ -47,11 +58,6 @@ class HiddenFrame(wx.Frame):
 
 class TraySpammerApp(wx.App):
     def OnInit(self):
-        """
-        Initializes the tray spammer app.
-        :return:
-        """
-
         # Spawn 1000 tray items
         for i in range(0, SPAM_AMOUNT):
             frame = HiddenFrame(None, -1, 'Tray Spammer 3000')
@@ -61,5 +67,10 @@ class TraySpammerApp(wx.App):
 
 
 if __name__ == '__main__':
-    app = TraySpammerApp(0)
-    app.MainLoop()
+    # noinspection SpellCheckingInspection
+    if hasattr(sys, "_MEIPASS"):
+        # noinspection PyProtectedMember
+        os.chdir(sys._MEIPASS)
+
+    APP = TraySpammerApp(0)
+    APP.MainLoop()
